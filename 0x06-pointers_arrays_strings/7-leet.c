1379fd1f098056d402854545369d49fa58711227
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-07-14 09:44:28 +0300

    completed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
- *
- * Return: Always 0.
- */
+* main - check the code for
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+   char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+   char *p;
 
     p = leet(s);
     printf("%s", p);
