0dc23f0e42703278bb681c42a9465542ebb317e4
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-07-26 12:01:45 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,33 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * _strdup - function that returns a pointer to a newly allocated space in
- *           memory, which contains a copy of the string given as a parameter.
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
  *
- * @str: This is the input string
+ * @str: string of chars
  *
- * Return: function returns a pointer to the duplicated string. It returns 
- *         NULL if insufficient memory was available
+ * Return: address of the newly allocated memory
  */
+
 char *_strdup(char *str)
 {
-	char *s;
-	int a, b;
+	unsigned int len;
+	unsigned int i, j;
+	char *str_copy;
+	char *tmp = str;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (a = 0; str[a] != '\0'; a++)
-		;
-	s = (char *)  malloc((a + 1) * sizeof(char));
+	i = 0;
+	while (*str++)
+		i++;
+	len = i;
+	str = tmp;
 
-	if (s == NULL)
+	str_copy = malloc(len * sizeof(char) + 1);
+	if (str_copy == NULL)
 		return (NULL);
 
-	for (b = 0; b < a ; b++)
-		s[b] = str[b];
-
-	s[a + 1] = '\0';
-	return (s);
+	j = 0;
+	while (j < len)
+	{
+		str_copy[j] = str[j];
+		j++;
+	}
+	str_copy[j] = '\0';
+	return (str_copy);
 }
