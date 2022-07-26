406083e9b34c004eb5e22cb71cc201208dae0146
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-07-26 11:58:44 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,56 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - function that concatenates two strings.
- *              if NULL is passed, treat it as an empty string
- *              The function should return NULL on failure
+ * str_concat - function that concatenates two strings
  *
- * @s1: This is the output string
- * @s2: This is the input string
+ * @s1: string of chars
+ * @s2: string of chars
  *
- * Return: The returned pointer should point to a newly allocated space in
- *         memory which contains the contents of s1, followed by the contents 
- *         of s2, and null terminated
+ * Return: address of the newly allocated memory
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	char *s;
+	unsigned int len1, len2;
+	unsigned int i, j;
+	char *str_copy;
+	char *tmp1 = s1;
+	char *tmp2 = s2;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
 
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
+	i = 0;
+	while (*s1++)
+		i++;
+	len1 = i;
+	s1 = tmp1;
 
-	if (s == NULL)
+	i = 0;
+	while (*s2++)
+		i++;
+	len2 = i;
+	s2 = tmp2;
+
+	str_copy = malloc((len1 + len2) * sizeof(char) + 1);
+	if (str_copy == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
+
+	j = 0;
+	while (j < len1)
 	{
-		s[a] = s1[a];
+		str_copy[j] = s1[j];
+		j++;
 	}
-	for (b = 0; s2[b] != '\0'; b++)
+	while (j < len1 + len2)
 	{
-		s[a] = s2[b];
-		a++;
+		str_copy[j] = s2[j - len1];
+		j++;
 	}
-	return (s);
+	str_copy[j] = '\0';
+	return (str_copy);
 }
