24a54c3b7250c246a0add55d7dd98a9d906a7431
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-03 14:39:24 +0300

    completed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - function that prints a name.
+ * print_name-function that prints a name.
  * @f: function pointer
  * @name: name
  * Return: nothing
