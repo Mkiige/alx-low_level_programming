7f63b7ea31683d6f00128a817d62d60c73068fc7
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-11 14:59:09 +0300

    completed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "lists.h"
 /**
  * _strlen - finding the lenght of a string.
@@ -38,43 +37,3 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (*head);
 }
-=======
-#include "lists.h"
-/**
- * _strlen - finding the lenght of a string.
- * @s: first pointer
- * Return: it returns the length of the string.
- */
-int _strlen(char *s)
-{
-	int str;
-
-	str = 0;
-	while (s[str] != '\0')
-	{
-		str++;
-	}
-	return (str);
-/**
-* add_node -  adds a new node at the beginning of a list_t list.
-* @head: pointer to the addres of head
-* @str: pointer to the string of each new node
-* Return: addres to the new head node
-*/
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-
-	if (!head)
-		return (0);
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (0);
-	new->str = strdup(str);
-	new->len = _strlen((char *)str);
-	new->next = *head;
-	*head = new;
-
-	return (*head);
-}
->>>>>>> 23542e9c93d28b2bf35220daa57736670aae9722
