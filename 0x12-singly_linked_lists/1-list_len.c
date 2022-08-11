99ff9ba7d900b7007fb72e3f68fe6a49aeeb5703
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-11 13:23:25 +0300

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 
-**
+/**
  * listint_len - Returns the number of elements
  *               in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
