cc2c870bbf48b31c7a588f8f1de9adf3cc26f77b
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-11 13:57:25 +0300

    print_list
    /**
     * print_listint - Prints all the elements of a listint_t list.
     * @h: A pointer to the head of the list_t list.
     *
     * Return: The number of nodes in the list_t list.
     */

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,5 @@
 #include "lists.h"
 
-/**
- * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the head of the list_t list.
- *
- * Return: The number of nodes in the list_t list.
- */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
