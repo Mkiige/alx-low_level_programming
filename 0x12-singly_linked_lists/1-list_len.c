389745bb811f391a85177265b92f8dc64b6e9284
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-08-11 13:13:07 +0300

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * list_len - Returns the number of elements
- *               in a linked list_t list.
- * @h: A pointer to the head of the list_t list.
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
  *
- * Return: The number of elements in the list_t list.
+ * Return: The number of elements in the listint_t list.
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
 
